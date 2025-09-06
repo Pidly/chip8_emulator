4eb2b609707cf68e37bd17a9e79c60d57c1cb002
pidly <ji773rs@gmail.com>
2025-09-06 10:57:15 -0700

    Draw something to the screen

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *sdlWindow = SDL_CreateWindow("chip8 emulator", 0, 0, 1000, 500, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_ACCELERATED);
 
+    //Clear screen
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_Rect rect = SDL_Rect(0, 0, 100, 150);
+    SDL_RenderFillRect(renderer, &rect);
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(sdlWindow);
     SDL_Quit();
     return 0;
 }
