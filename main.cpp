4bb4e617a795d6332bd2257d1b44f5b0d7d83fd6
pidly <ji773rs@gmail.com>
2025-09-06 12:09:45 -0700

    Changed the window to chip8 apect ratio

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *sdlWindow = SDL_CreateWindow("chip8 emulator", 0, 0, 1000, 500, SDL_WINDOW_SHOWN);
+    //chip8 64x32 display
+    SDL_Window *sdlWindow = SDL_CreateWindow("chip8 emulator", 0, 0, 640, 320, SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_ACCELERATED);
 
     //Clear screen
     SDL_RenderClear(renderer);
+
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     SDL_Rect rect = SDL_Rect(0, 0, 100, 150);
     SDL_RenderFillRect(renderer, &rect);
